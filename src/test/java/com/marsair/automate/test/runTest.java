19af3cdf0aa7e3ded4bc7153c1b7421471eccb03
maikeffi <maikeffi@gmail.com>
2016-08-09 22:20:17 +0530

    First commit

---
+++ src/test/java/com/marsair/automate/test/runTest.java
@@ -0,0 +1,15 @@
+package com.marsair.automate.test;
+
+import org.junit.Test;
+
+/**
+ * Created by Ganga Hawa on 09-08-2016.
+ */
+public class runTest {
+
+
+    @Test
+    public void run(){
+        System.out.println("Test");
+    }
+}
